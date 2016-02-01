e6f08539a725da77d936837c5511f77bab0003f3
Kelvin van Amstel <k.v.amstel@hotmail.com>
2016-02-01 18:04:22 +0100

    Finished "voor beoordeling"
    Alleen voor de beoordeling is mijn portfolio zo afgerond.
    Begin pagina ook verwerken voor andere pagina's

--- js/script.js
+++ js/script.js
@@ -7,9 +7,6 @@ var linkedin = document.querySelector('#li');
 var github = document.querySelector('#gi');
 var mail = document.querySelector('#ma');
 
-var link = document.querySelector('#link');
-var gith = document.querySelector('#gith');
-var ma = document.querySelector('#mail');
 
 if (width.matches) {
     h1.classList.remove('invisible');
@@ -49,30 +46,6 @@ function hover6(){
     document.getElementById("ma").src='./img/mail1.png';
 }
 
-function hover7(){
-    document.getElementById("link").src='./img/link_icon_a.jpg';
-}
-
-function hover8(){
-    document.getElementById("link").src='./img/link_icon.jpg';
-}
-
-function hover9(){
-    document.getElementById("gith").src='./img/github_icon_a.jpg';
-}
-
-function hover10(){
-    document.getElementById("gith").src='./img/github_icon.jpg';
-}
-
-function hover11(){
-    document.getElementById("mail").src='./img/mail_icon_a.jpg';
-}
-
-function hover12(){
-    document.getElementById("mail").src='./img/mail_icon.jpg';
-}
-
 h1.addEventListener('click', show);
 linkedin.addEventListener('mouseover', hover1);
 linkedin.addEventListener('mouseout', hover2);
@@ -80,10 +53,3 @@ github.addEventListener('mouseover', hover3);
 github.addEventListener('mouseout', hover4);
 mail.addEventListener('mouseover', hover5);
 mail.addEventListener('mouseout', hover6);
-
-link.addEventListener('mouseover', hover7);
-link.addEventListener('mouseout', hover8);
-gith.addEventListener('mouseover', hover9);
-gith.addEventListener('mouseout', hover10);
-ma.addEventListener('mouseover', hover11);
-ma.addEventListener('mouseout', hover12);
